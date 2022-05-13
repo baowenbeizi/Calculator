0a4986b8ddd24502f204d89e362d5085c99448bd
rongjielin <13459666975@163.com>
2022-05-13 10:46:08 +0800

    绘制UI界面

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+//    val mTvMain: TextView = TODO()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+//        val x = 10
+//        val y = 9
+//        if (x in 1..y+1) {
+//            Log.d("123", "fits in range")
+//            println()
+//        }
+        //sampleEnd
+//        val fruits = listOf("banana", "avocado", "apple", "kiwifruit")
+//        fruits
+//            .filter { it.startsWith("a") }
+//            .sortedBy { it }
+//            .map { it.uppercase() }
+//            .forEach { println(it) }
     }
 }
